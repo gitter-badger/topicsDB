b6d0b002b54a11d682d3be033ccc263a2a105cd3
Marcos Antônio de Souza Silva <chuckymass@gmail.com>
2015-04-14 11:09:16 -0300

    Gerando arquivo ddl apartir do .SAS

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-DIR = '../'
+DIR = ''
 PATH =  DIR 
 DADOS = PATH + '/DADOS'
-#dado retirado da tabela de refencia do dados aberto para docente
+INPUT = PATH + 'INPUTS/'
 
+#dado retirado da tabela de refencia do dados aberto para docente
 TS_DOCENTES_REF = [5, 13, 3, 3, 5, 4, 1, 1, 1, 4, 3, 2, 9, 3, 2, 9, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 3, 6, 2, 2, 5, 5, 2, 
 100, 9, 1, 4, 6, 2, 2, 5, 5, 2, 100, 9, 1, 4, 6, 2, 2, 5, 5, 2, 100, 9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 11, 3, 3, 4,
@@ -18,9 +19,11 @@ DNR = DADOS + '/Docentes Norte'
 DSD = DADOS + '/Docentes Sudeste'
 DSU = DADOS + '/Docentes Sul'
 ESC = DADOS + '/ESCOLAS'
+
 repositorio = { 
 	'PATH' : PATH,
 	'DADOS' : DADOS,
+	'INPUT' : INPUT,
 	'DOCENTES': [DCO, DND, DNR, DSD, DSU],
 	'ESCOLAS' :	[ESC],
 	'REFERENCIA' : [TS_DOCENTES_REF,TS_ESCOLA]
@@ -48,26 +51,94 @@ def miner(line, ref):
 		txt = txt[ref[a]:]
 		a += 1
 	print campos[2]
+
+def generatorDDL(path, f):
+
+	arq = open(path + "/" + f,'r')
+
+	txt = ''.join(arq.readlines())
+	txt = txt[txt.index('INPUT\r\n') + 7 : ]
+	
+	ddl = []
+	ddl.append("--")
+	ddl.append("-- " + f)
+	ddl.append("--")
+	ddl.append("CREATE TABLE " + f[:-4].lower() + "(")
+
+	FKS = []
+	PKS = [] # Nao eh possivel reestabelecer a chave estrangeira
+
+	# gerando as colunas 
+	for line in txt.split("\n") :
+		campos = line.split("\t")
+		if len(campos) == 4 :
+
+			n_campo = campos[1].lower()
+
+			if n_campo.startswith('fk_') :
+				FKS.append(n_campo)
+		
+			if n_campo.startswith('pk_') :
+				PKS.append(n_campo)
+
+			info_c = n_campo.strip()
+			if campos[2][0] != '$' : 
+				info_c += ' numeric'
+			else :
+				info_c += ' varchar'
+			info_c += ' NOT NULL,'
+
+			ddl.append(info_c)
+
+	# gerando a lista de chaves primarias
+	pk_nome = "pk"
+	pk_campos = ""
+	i = 0
+	for pk in PKS :
+		if i != 0 :
+			pk_campos += ", "
+		pk_nome += pk.replace("pk","")
+		pk_campos += pk
+		i += 1
+
+	ddl.append("CONSTRAINT " + pk_nome + " PRIMARY KEY(" + pk_campos.strip() + ")")
+
+	ddl.append(");")
+	# print ddl
+	# print FKS
+	
+	return {'DDL' : ddl, 'FKS' : FKS}
+
 from os import listdir
 from os.path import isfile, join, exists
 
 def main():
+	
+	ddls = []
+	for f in listdir(repositorio['INPUT']) :
+		if isfile(join(repositorio['INPUT'],f)) :
+			ddls.append(generatorDDL(repositorio['INPUT'], f))
 
-	# for mypath in repositorio['DOCENTES'] :
+	for element in ddls:
+		k = 0
+		t = len(element['DDL'])
+		for ddl in element['DDL']:
+			imp = ""
+			if k > 3 and k != t - 1:
+				imp += '\t'
+			imp += ddl
+			print imp
+			k += 1
+		print ''
+	# for mypath in repositorio['ESCOLAS'] :
 	# 	if exists(mypath) :
 	# 		for f in listdir(mypath) :
 	# 			if isfile(join(mypath,f)) :
 	# 				print "#FILE: " + f; 
-	# 				miner(open(mypath + "/" + f,'r'), repositorio["REFERENCIA"][0])
-	for mypath in repositorio['ESCOLAS'] :
-		if exists(mypath) :
-			for f in listdir(mypath) :
-				if isfile(join(mypath,f)) :
-					print "#FILE: " + f; 
-					arq = open(mypath + "/" + f,'r')
-					lines = arq.readlines()
-					for ln in lines:
-						miner(ln, repositorio["REFERENCIA"][1])
+	# 				arq = open(mypath + "/" + f,'r')
+	# 				lines = arq.readlines()
+	# 				for ln in lines:
+	# 					miner(ln, repositorio["REFERENCIA"][1])
 
 
 main()
